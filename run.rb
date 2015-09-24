102cb57730dfe75b75056e348e6ccf5cdfe93f15
Jonathan Arp <jarp@nd.edu>
2015-09-24 11:16:19 -0400

    added an example of looping in an erb file

--- run.rb
+++ run.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 
 get '/' do
+  # hello
   @varaible_for_view = 'Hi! I am a variable. the @ at the beginning will make it accessible in the erb/view file.'
+  @people = ["Jonathan", "Joel", "Jarrett"]
   erb :index, layout: :main
 end
 
 get '/years' do
+
+
+
   # code!
   erb :years, layout: :main
 end
